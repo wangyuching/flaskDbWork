ad39db78da8fd2e7e99cf614a89ba236902c1dd1
昱晴 王 <5b1g0023@stust.edu.tw>
2025-05-17 18:19:59 +0800

    0

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def login():
     return render_template('login.html')
+
+@app.route('/wp')
+def wp():
+    return render_template('wp.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

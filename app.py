65a6e4daa5f22f64e063e0475052630378b139a9
昱晴 王 <5b1g0023@stust.edu.tw>
2025-05-18 14:13:10 +0800

    1

--- app.py
+++ app.py
@@ -43,5 +43,25 @@ def up():
 
     return render_template('up.html', datas=datas, total=total)
 
+#@app.route('/submit', methods=['POST'])
+#def submit():
+    # 傳到up.html
+#    return render_template('Wp.html')
+
+#@app.route('/wp', methods=['GET'])
+#def login():
+#    return render_template('wp.html')
+
+#@app.route('/wp', methods=['POST'])
+#def wp():
+    #結束遊戲>紀錄>回傳紀錄關卡>紀錄回傳到up
+#    return redirect(url_for('up'))
+
+#@app.route('/up', methods=['GET'])
+#def wp():
+    #結束遊戲>紀錄>回傳紀錄關卡>紀錄回傳到up
+#    return  render_template('up.html')
+
+
 if __name__ == '__main__':
     app.run()

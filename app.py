855a45e17809c64bffb0661720bb279c422e3235
昱晴 王 <5b1g0023@stust.edu.tw>
2025-05-18 11:05:02 +0800

    connect succese

--- app.py
+++ app.py
@@ -2,18 +2,28 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def login():
     return render_template('login.html')
 
-@app.route('/wp')
-def wp():
-    return render_template('wp.html')
-
 @app.route('/submit', methods=['POST'])
 def submit():
-    return redirect(url_for('wp'))
+    # 傳到up.html
+    return render_template('up.html')
 
+#@app.route('/wp', methods=['GET'])
+#def login():
+#    return render_template('wp.html')
+
+#@app.route('/wp', methods=['POST'])
+#def wp():
+    #結束遊戲>紀錄>回傳紀錄關卡>紀錄回傳到up
+#    return redirect(url_for('up'))
+
+#@app.route('/up', methods=['GET'])
+#def wp():
+    #結束遊戲>紀錄>回傳紀錄關卡>紀錄回傳到up
+#    return  render_template('up.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
